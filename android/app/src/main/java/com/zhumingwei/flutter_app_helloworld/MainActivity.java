830b92d56d48ba73525b49f027cc39ebb9ac9580
zhumingwei <zdf312192599@163.com>
2019-11-24 20:10:48 +0800

    init

---
+++ android/app/src/main/java/com/zhumingwei/flutter_app_helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zhumingwei.flutter_app_helloworld;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
