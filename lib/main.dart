ca71fed4cb416a348ff00890ab62f4afacc2b97d
zhumingwei <zdf312192599@163.com>
2019-11-25 15:29:04 +0800

    Padding

--- lib/main.dart
+++ lib/main.dart
@@ -26,21 +26,9 @@ class MyView extends StatelessWidget {
       appBar: AppBar(
         title: Text("title"),
       ),
-      body: Container(
+      body: Padding(
+        padding: EdgeInsets.all(44.0),
         child: Text('Container（容器）在UI框架中是一个很常见的概念，Flutter也不例外。'),
-        padding: EdgeInsets.all(18.0),
-        // 内边距
-        margin: EdgeInsets.all(44.0),
-        // 外边距
-        width: 180.0,
-        height: 240,
-        alignment: Alignment.center,
-        // 子Widget居中对齐
-        decoration: BoxDecoration(
-          //Container样式
-          color: Colors.red, // 背景色
-          borderRadius: BorderRadius.circular(10.0), // 圆角边框
-        ),
       ),
     );
   }
