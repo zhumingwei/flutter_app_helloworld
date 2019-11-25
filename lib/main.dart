6e7c1e6263b0a1c6b053d38450e620fe253dfcee
zhumingwei <zdf312192599@163.com>
2019-11-25 13:16:48 +0800

    文字

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Center(
-        child: Text(
-          '文本是视图系统中的常见控件，用来显示一段特定样式的字符串，就比如Android里的TextView，或是iOS中的UILab',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,color: Colors.red),
-        ),
-      )
+      home: MyView()
     );
   }
+}
+
+class MyView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '文本是视图系统中的常见控件，用来显示一段特定样式的字符串，就比如Android里的TextView，或是iOS中的UILab',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,color: Colors.red),
+      ),
+    );
+  }
+
 }
