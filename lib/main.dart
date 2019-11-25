c2f194f8029751d67c8d333f266e9969932647ab
zhumingwei <zdf312192599@163.com>
2019-11-25 13:49:32 +0800

    listview_direcetion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_hello/page1.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,27 +6,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyView()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyView());
   }
 }
 
-class MyView extends StatelessWidget{
-
+class MyView extends StatelessWidget {
   TextStyle blackStyle = TextStyle(
-      fontWeight:FontWeight.normal,
-      fontSize: 20,
-      color:Colors.black
-  );
-  TextStyle redStyle = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 20,
-    color: Colors.red
-  );
+      fontWeight: FontWeight.normal, fontSize: 20, color: Colors.black);
+  TextStyle redStyle =
+      TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.red);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,13 +27,17 @@ class MyView extends StatelessWidget{
         title: Text("title"),
       ),
       body: ListView(
+        scrollDirection: Axis.horizontal,
+        itemExtent: 140,
         children: <Widget>[
-          ListTile(leading: Icon(Icons.map),title: Text('Map')),
-          ListTile(leading: Icon(Icons.mail),title: Text('Mail'),),
-          ListTile(leading: Icon(Icons.message),title: Text('Message'),)
+          Container(color: Colors.black),
+          Container(color: Colors.red),
+          Container(color: Colors.blue),
+          Container(color: Colors.green),
+          Container(color: Colors.yellow),
+          Container(color: Colors.orange),
         ],
       ),
     );
   }
-
 }
