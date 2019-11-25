0a596ff849852576aedd2ed3e39ccb7fb0edc4a1
zhumingwei <zdf312192599@163.com>
2019-11-25 13:32:15 +0800

    富文本

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,36 @@ class MyApp extends StatelessWidget {
 }
 
 class MyView extends StatelessWidget{
+
+  TextStyle blackStyle = TextStyle(
+      fontWeight:FontWeight.normal,
+      fontSize: 20,
+      color:Colors.black
+  );
+  TextStyle redStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+    color: Colors.red
+  );
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        '文本是视图系统中的常见控件，用来显示一段特定样式的字符串，就比如Android里的TextView，或是iOS中的UILab',
-        textAlign: TextAlign.center,
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,color: Colors.red),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("title"),
+      ),
+      body: Center(
+        child: Text.rich(
+          TextSpan(
+              children: <TextSpan>[
+                TextSpan(text: '文本是视图系统中常见的控件，它用来显示一段特定样式的字符串，类似',style: redStyle),
+                TextSpan(text: "Android", style: blackStyle),
+                TextSpan(text:'中的', style:redStyle),
+                TextSpan(text:'TextView', style: blackStyle)
+              ]
+          ),
+          textAlign: TextAlign.center,
+
+        ),
       ),
     );
   }
