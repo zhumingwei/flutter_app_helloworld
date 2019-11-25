c55e781793a1711c66267b918be2e13888704253
zhumingwei <zdf312192599@163.com>
2019-11-25 13:35:07 +0800

    图

--- lib/main.dart
+++ lib/main.dart
@@ -35,17 +35,8 @@ class MyView extends StatelessWidget{
         title: Text("title"),
       ),
       body: Center(
-        child: Text.rich(
-          TextSpan(
-              children: <TextSpan>[
-                TextSpan(text: '文本是视图系统中常见的控件，它用来显示一段特定样式的字符串，类似',style: redStyle),
-                TextSpan(text: "Android", style: blackStyle),
-                TextSpan(text:'中的', style:redStyle),
-                TextSpan(text:'TextView', style: blackStyle)
-              ]
-          ),
-          textAlign: TextAlign.center,
-
+        child: Image.network(
+        "https://is.gd/ePQGqe"
         ),
       ),
     );
