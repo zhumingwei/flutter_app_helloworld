86f22849441a1d570b232a17e9ec246c61050d6d
zhumingwei <zhumingwei2016@gmail.com>
2020-01-11 09:44:48 +0800

    animated Switcher

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(
+          title: "Flutter Demo Home Page",
+        ));
+  }
+}
+
+const platform = MethodChannel('samples.chenhang/navigation');
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellowAccent,
+      appBar: AppBar(title: Text("Default Page")),
+      body: Center(
+        child: AnimatedSwitcherCounterRoute(),
+      ),
+    );
+  }
+}
+
+class AnimatedSwitcherCounterRoute extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AnimatedSwitcherCounterRouteState();
+}
+
+class _AnimatedSwitcherCounterRouteState
+    extends State<AnimatedSwitcherCounterRoute> {
+  int _count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return ScaleTransition(
+                child: child,
+                scale: animation,
+              );
+            },
+            child: Text('$_count',
+            key: ValueKey(_count),
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ),
+          RaisedButton(
+            child: const Text('+1'),
+            onPressed: (){
+              setState(() {
+                _count+=1;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
