ba43b81ce9739a36a13287f9550822c71331593e
zhumingwei <zhumingwei2016@gmail.com>
2020-03-27 22:05:25 +0800

    有element绘制

--- flutter/flutter_library/lib/main.dart
+++ flutter/flutter_library/lib/main.dart
@@ -36,74 +36,43 @@ class MyHomePage extends StatelessWidget {
       backgroundColor: Colors.yellowAccent,
       appBar: AppBar(title: Text("Default Page")),
       body: Center(
-        child: _buildCustomPaint(),
+        child: CustomHome(),
       ),
     );
   }
 
-  Widget _buildCustomPaint() {
-    return CustomPaint(
-      size: Size(300, 300),
-      painter: MyPainter(),
-      child: null,
+
+}
+
+class CustomHome extends Widget {
+  @override
+  Element createElement() {
+    return HomeView(this);
+  }
+}
+
+class HomeView extends ComponentElement{
+  HomeView(Widget widget) : super(widget);
+  String text = "123456789";
+
+  @override
+  Widget build() {
+    Color primary=Theme.of(this).primaryColor; //1
+    return GestureDetector(
+      child: Center(
+        child: FlatButton(
+          child: Text(text, style: TextStyle(color: primary),),
+          onPressed: () {
+            var t = text.split("")..shuffle();
+            text = t.join();
+            markNeedsBuild(); //点击后将该Element标记为dirty，Element将会rebuild
+          },
+        ),
+      ),
     );
   }
 }
 
-class MyPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    double eWidth = size.width / 15;
-    double eHeight = size.height / 15;
-
-    //画棋盘背景
-    var paint = Paint()
-      ..isAntiAlias = true
-      ..style = PaintingStyle.fill //填充
-      ..color = Color(0x77cdb175); //背景为纸黄色
-    canvas.drawRect(Offset.zero & size, paint);
-
-    //画棋盘网格
-    paint
-      ..style = PaintingStyle.stroke //线
-      ..color = Colors.black87
-      ..strokeWidth = 1.0;
-
-    for (int i = 0; i <= 15; ++i) {
-      double dy = eHeight * i;
-      canvas.drawLine(Offset(0, dy), Offset(size.width, dy), paint);
-    }
-
-    for (int i = 0; i <= 15; ++i) {
-      double dx = eWidth * i;
-      canvas.drawLine(Offset(dx, 0), Offset(dx, size.height), paint);
-    }
-
-    //画一个黑子
-    paint
-      ..style = PaintingStyle.fill
-      ..color = Colors.black;
-    canvas.drawCircle(
-      Offset(size.width / 2 - eWidth / 2, size.height / 2 - eHeight / 2),
-      min(eWidth / 2, eHeight / 2) - 2,
-      paint,
-    );
-
-    //画一个白子
-    paint.color = Colors.white;
-    canvas.drawCircle(
-      Offset(size.width / 2 + eWidth / 2, size.height / 2 - eHeight / 2),
-      min(eWidth / 2, eHeight / 2) - 2,
-      paint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
-
-}
 
 
 
